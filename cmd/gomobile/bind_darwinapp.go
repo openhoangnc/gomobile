dad57bfc6cceb2f844e351855c0e715702dcfb88
Randy Reddig <randy@alta.software>
2021-07-14 22:10:18 -0700

    cmd/gomobile: just use arch instead of parsing GOARCH from env

--- cmd/gomobile/bind_darwinapp.go
+++ cmd/gomobile/bind_darwinapp.go
@@ -93,7 +93,7 @@ func goDarwinbind(gobind string, pkgs []*packages.Package, targetPlatforms, targ
 
 			env := darwinEnv[platform+"/"+arch]
 
-			if err := writeGoMod(platform, getenv(env, "GOARCH")); err != nil {
+			if err := writeGoMod(platform, arch); err != nil {
 				return err
 			}
 
